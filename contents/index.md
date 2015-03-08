8d83e0744ab2e0b4b250bb761695f22116534b22
Para4 Company Laptop <para4companylaptop@reshut1-52-28-dhcp.int.colorado.edu>
2015-03-08 16:18:14 -0600

    initial commit, folders and files set up

---
+++ contents/index.md
@@ -0,0 +1,4 @@
+---
+title: Weather
+template: layout.jade
+---
---
+++ contents/js/app.js
@@ -0,0 +1,7 @@
+$("#currentConditions").on("click", function(event){
+	currentConditions.load()
+})
+
+$("#forecast").on("click", function(event){
+	forecast.load()
+})
